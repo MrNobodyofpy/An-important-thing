08c7949f383a5d071c58344a2b74e16f02effb35
unknown <phanlehoangxzc@gmail.com>
2023-12-18 13:10:22 +0700

    file 4 commit

---
+++ Password Machine.py
@@ -0,0 +1,13 @@
+import pygame
+
+print("Hello, New user")
+print("Do you want to start this program")
+Answer = Input()
+if Answer == Yes:
+    print("Hohoho, there is no program here.")
+else:
+    print("Smart kid.")
+
+print("I will complete this project after I pass the exam : )")
+
+    
