bffad6b0b84cec30d783c6751151a345c8bedacc
unknown <phanlehoangxzc@gmail.com>
2023-12-19 20:34:41 +0700

    file_n3

--- My Data.py
+++ My Data.py
@@ -6,5 +6,8 @@ print("Do you want to play? Yes or No?")
 A = input()
 if A == "Yes":
 	print("Good, but there is no game here")
-else:
+elif A == "No":
 	print("Bruh")
+
+else:
+	print("Wrong answer")
