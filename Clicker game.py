bc2fccb50332e66004075e28d44b26492128cf9c
unknown <phanlehoangxzc@gmail.com>
2023-12-19 20:06:17 +0700

    1_commit

--- Clicker game.py
+++ Clicker game.py
@@ -2,3 +2,9 @@ import pygame
 
 print("I am really happy to work on this project")
 print("I will complete this project after I pass the exam : )")
+print("Do you want to play? Yes or No?")
+A = input()
+if A == "Yes":
+	print("Good, but there is no game here")
+else:
+	print("Bruh")
