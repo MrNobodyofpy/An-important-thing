17a61be980dfccbd9081674b8c1687c1c7bcc948
unknown <phanlehoangxzc@gmail.com>
2023-12-21 13:17:51 +0700

    something1

--- Clicker game.py
+++ Clicker game.py
@@ -2,7 +2,6 @@ import pygame
 
 print("I hope it will be a great game")
 
-print("I will complete this project after I pass the exam : )")
 print("Do you want to play? Yes or No?")
 A = input()
 if A == "Yes":
