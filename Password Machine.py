ea4766db6f9bef791c0ac38eaaaee0fbb9e3f7c7
unknown <phanlehoangxzc@gmail.com>
2024-01-13 19:59:37 +0700

    It_will_be_completed

--- Password Machine.py
+++ Password Machine.py
@@ -1,23 +1,3 @@
 import pygame
 
-va1 = "it's va1"
-print("I will complete this project tomorrow : )")
-
-print("Do you want to start this program")
-Answer = Input()c
-if Answer == Yes:
-    print("Hohoho, there is no program here.")
-else:
-    print("Smart kid.")
-
-print("Do you want to play? Yes or No?")
-A = input()
-if A == "Yes":
-    print("Good, but there is no game here")
-elif A == "No":
-    print("Bruh")
-
-else:
-    print("Wrong answer")
-
 print("It will be completed : )")
