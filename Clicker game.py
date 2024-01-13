2949f38a7e85912e0b80933e2d935e9fb186b4aa
unknown <phanlehoangxzc@gmail.com>
2024-01-13 19:59:23 +0700

    It_will_be_completed

--- Clicker game.py
+++ Clicker game.py
@@ -1,14 +1,3 @@
 import pygame
-va1 = "it's va1"
-print("I will complete this project tomorrow : )")
-print("Do you want to play? Yes or No?")
-A = input()
-if A == "Yes":
-	print("Good, but there is no game here")
-elif A == "No":
-	print("Bruh")
 
-else:
-	print("Wrong answer")
-	
 print("It will be completed : )")
