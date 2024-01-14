2d03d60cf44222e405090cdd21ba34e2c1bc61b0
unknown <phanlehoangxzc@gmail.com>
2024-01-14 18:09:13 +0700

    : )

--- Clock Machine.py
+++ Clock Machine.py
@@ -1,16 +1,20 @@
 import pygame
-
 pygame.init()
 
+#Set the size of the "window""
 screen = pygame.display.set_mode((500,600))
 
+#Set name of colors : )
 GREY = (120, 120, 120)
 
+#Set "is_running" turn on first : )
 is_running = True
 
+#Start running the program : )
 while is_running:
 	screen.fill(GREY)
 
+#Event while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			is_running = False
