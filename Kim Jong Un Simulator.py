593564952b84af228d6f2ac313b582cc94ecc204
unknown <phanlehoangxzc@gmail.com>
2024-06-26 08:09:23 +0700

    Completed

--- Kim Jong Un Simulator.py
+++ Kim Jong Un Simulator.py
@@ -22,6 +22,9 @@ Clicks = 0
 # Set "is_running" turn on first
 is_running = True
 
+#Message from Kim family
+message = ""
+
 # Define button_rect before the loop
 button_rect = button_image.get_rect()
 button_rect.center = (window_width // 2, window_height // 2)
@@ -40,12 +43,26 @@ while is_running:
                 Click_score += 1
                 Clicks += 1
 
+    #You did it!
+                if Click_score == 10:
+                    message = "You destroyed the Humanity!"
+                elif Click_score == 100:
+                    message = "You destroyed the Earth"
+                elif Click_score == 1000:
+                    message = "You don't have any more bombs"
+                elif Click_score == 10000:
+                    message = "Nothing exists anymore"
+
     # Draw time
     screen.blit(button_image, button_rect)
     font = pygame.font.Font(None, 36)
     text = font.render(f"Score: {Click_score}", True, WHITE)
     screen.blit(text, (10, 10))
 
+    #Message
+    if message:
+        message_text = font.render(message, True, WHITE)
+        screen.blit(message_text, (10, 50))
     pygame.display.update()
 
 pygame.quit()
